5f155bf6812ee840ef330ea4510e6421ed61f24a
Jonathan Vernersson <jonathan.vernersson@student.kyh.se>
2020-10-07 11:31:56 +0200

    Started with player character class

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import pygame
+
 pygame.init()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, image):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(image, (37, 20))
+
 
 def main():
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Drunk Frogger")
     running = True
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
