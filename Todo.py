f29f91794200f11bc80ae945915fd76fec895454
Andblad <tobbe_andblad@hotmail.com>
2020-10-08 09:39:29 +0200

    addlist function

--- Todo.py
+++ Todo.py
@@ -3,12 +3,14 @@ from pathlib import Path
 
 def main():
     p = Path("Todo.txt")
-    Todo_list = Todo.read_text(encoding="utf8").splitlines()
+    todo_list = p.read_text(encoding="utf8").splitlines()
 
     while True:
-        meny = int(input("Välj ett meny val:\n1: Titta på Todolistan"))
+        meny = int(input("Välj ett meny val:\n1: Titta på Todolistan\n2: Lägg till i Todo-list"))
         if meny == 1:
-            showlist(Todo_list)
+            showlist(todo_list)
+        if meny == 2:
+            todo_list = addlist(todo_list,p)
 
 
 def showlist(text):
@@ -18,10 +20,12 @@ def showlist(text):
         print(f"{number}: {line}")
 
 
-def addlist(Todo_list):
-    new_content = input("Vad vill du lägga till?")
-    Todo_list.append(new_content)
+def addlist(todo_list,p):
 
+    new_content = input("Vad vill du lägga till?")
+    todo_list.append(new_content)
+    p.write_text("\n".join(todo_list), encoding='utf8')
+    return todo_list
 
 if __name__ == "__main__":
     main()
