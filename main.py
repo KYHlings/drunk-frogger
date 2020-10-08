719cc13940783301bc6b1d24f3ab84595c5da0dc
david <david.pergament@student.kyh.se>
2020-10-08 13:22:46 +0200

    flyttar sprite

--- main.py
+++ main.py
@@ -16,26 +16,30 @@ def main():
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Drunk Frogger")
     running = True
-    screen.fill((0, 0, 0))
+
     while running:
+        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.K_ESCAPE:
+                running = False
+
+
+        velocity = 3
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player_x -= velocity
+        if keys[pygame.K_RIGHT]:
+            player_x += velocity
+        if keys[pygame.K_DOWN]:
+            player_y += velocity
+        if keys[pygame.K_UP]:
+            player_y -= velocity
+
         screen.blit(get_player_sprite(), (player_x, player_y))
         pygame.display.update()
-    velocity = 10
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player_x -= velocity
-    if keys[pygame.K_RIGHT]:
-        player_x += velocity
-    if keys[pygame.K_DOWN]:
-        player_y += velocity
-    if keys[pygame.K_UP]:
-        player_y -= velocity
-
-
 
 if __name__ == '__main__':
     main()
