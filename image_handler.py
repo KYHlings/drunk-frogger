7eb54ed504c8c361597968a629be2d4d56b682b9
Andblad <tobbe_andblad@hotmail.com>
2020-10-08 14:48:19 +0200

    add mob.

--- image_handler.py
+++ image_handler.py
@@ -11,3 +11,8 @@ def get_background_image():
     background_image = pygame.image.load("images_src/back_placehold2.png")
     background_image = pygame.transform.scale(background_image, (800, 600))
     return background_image
+
+def get_mob_sprite():
+    mob_sprite = pygame.image.load("images_src/lorry.png")
+    mob_sprite = pygame.transform.scale(mob_sprite,(80,40))
+    return mob_sprite
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from image_handler import get_player_sprite, get_background_image
+from image_handler import get_player_sprite, get_background_image, get_mob_sprite
 
 pygame.init()
 
@@ -13,11 +13,14 @@ class Player(pygame.sprite.Sprite):
 def main():
     player_x = 400
     player_y = 570
+    mob_x = 0
+    mob_y = 350
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Drunk Frogger")
     running = True
 
     while running:
+        screen.blit(get_background_image(), (0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -36,7 +39,7 @@ def main():
         if keys[pygame.K_ESCAPE]:
             running = False
 
-        screen.blit(get_background_image(), (0, 0))
+        screen.blit(get_mob_sprite(),(mob_x,mob_y))
         screen.blit(get_player_sprite(), (player_x, player_y))
         pygame.display.update()
 
