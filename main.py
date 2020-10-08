e1cf79d0cc540f259defe4a23c83191d79980364
Andblad <tobbe_andblad@hotmail.com>
2020-10-08 11:52:59 +0200

    implement player_control

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ class Player(pygame.sprite.Sprite):
 
 
 def main():
+    player_x = 400
+    player_y = 300
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Drunk Frogger")
     running = True
@@ -19,14 +21,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.blit(get_player_sprite(), (400, 300))
+        screen.blit(get_player_sprite(), (player_x, player_y))
         pygame.display.update()
     velocity = 10
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] :
-        pass
-
+    if keys[pygame.K_LEFT]:
+        player_x -= velocity
+    if keys[pygame.K_RIGHT]:
+        player_x += velocity
+    if keys[pygame.K_DOWN]:
+        player_y += velocity
+    if keys[pygame.K_UP]:
+        player_y -= velocity
 
 
 
