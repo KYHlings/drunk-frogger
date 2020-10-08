644bc177fc7a9aa40ba639e0e9b2cb26f34e723d
Jonathan Vernersson <jonathan.vernersson@student.kyh.se>
2020-10-08 11:47:48 +0200

    Started on character movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from image_handler import get_player_sprite
+
 pygame.init()
 
 
@@ -18,9 +19,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.blit(get_player_sprite(),(400,300))
-
+        screen.blit(get_player_sprite(), (400, 300))
         pygame.display.update()
+    velocity = 10
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] :
+        pass
+
+
 
 
 if __name__ == '__main__':
