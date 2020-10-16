ee637df09379e247ba42fbed93cbcd96f9741820
Daniel <daniel.yngve@student.kyh.se>
2020-10-16 15:53:17 +0200

    settings menu

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from game_loop import game_loop
-from start_menu import main_menu
+from start_menu import start_menu
 from sound_handler import Sound_fx
 
 
 def main():
+    volume = 0.1
     sound_fx = Sound_fx()
-    main_menu(sound_fx)
+    start_menu(sound_fx, volume)
     game_loop(sound_fx)
 
 
