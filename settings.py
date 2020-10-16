ca1e971ed988936ca0724c861f2548439807b5a2
Jonathan Vernersson <jonathan.vernersson@student.kyh.se>
2020-10-16 14:40:27 +0200

    Started on volume rect

--- settings.py
+++ settings.py
@@ -6,19 +6,23 @@ from window_handler import draw_text, font, text_colour, screen
 
 def settings():
     running = True
+    volume_int = 10
     while running:
+        volume_rect_x = 200
         screen.blit(main_menu_image(), (0, 0))
         draw_text("Settings", font, text_colour, screen, 210, 100)
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    running = False
+                    volume_int -= 5
                 if event.key == pygame.K_RIGHT:
-                    pass
+                    volume_int += 5
             if event.type == pygame.QUIT:
                 running = False
-        button_2 = pygame.Rect(235, 320, 0, 50)
-        pygame.draw.rect(screen, text_colour, button_2)
+        for i in range(volume_int):
+            volume_rect = pygame.Rect(volume_rect_x, 320, 0, 50)
+            volume_rect_x += 50
+            pygame.draw.rect(screen, text_colour, volume_rect)
         pygame.display.update()
 
 
