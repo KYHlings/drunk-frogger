4b5cbe968f64b3cd78897dbb3bb56e353cd1a167
david <david.pergament@student.kyh.se>
2020-10-20 09:47:40 +0200

    rotation fix

--- player.py
+++ player.py
@@ -20,6 +20,7 @@ class Player:
 
     def update_img(self):
         self.img = get_player_sprite(self.drunk_meter)
+        self.img = rotate_player_sprite(self.img,self.rotation)
         self.player_mask = pygame.mask.from_surface(self.img)
         self.player_rect = self.img.get_rect(topleft=(self.player_x,self.player_y))
         return self.img,self.player_rect
@@ -52,24 +53,20 @@ class Player:
         if keys[pygame.K_LEFT]  or keys[pygame.K_a]:
             self.player_x -= self.velocity
             self.rotation = 90
-            self.img = rotate_player_sprite(self.org_img,self.rotation)
             self.hitbox = (self.player_x + 2, self.player_y + 2, 27, 36)
             # pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
             self.player_x += self.velocity
             self.rotation = 270
-            self.img = rotate_player_sprite(self.org_img,self.rotation)
             self.hitbox = (self.player_x + 2, self.player_y + 2, 27, 36)
             # pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
         if keys[pygame.K_UP] or keys[pygame.K_w]:
             self.player_y -= self.velocity
             self.rotation = 0
-            self.img = rotate_player_sprite(self.org_img,self.rotation)
             self.hitbox = (self.player_x + 2, self.player_y + 2, 36, 27)
             # pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             self.rotation = 180
-            self.img = rotate_player_sprite(self.org_img,self.rotation)
             self.player_y += self.velocity
             self.hitbox = (self.player_x + 2, self.player_y + 2, 36, 27)
             # pygame.draw.rect(screen, (255, 0, 0), self.hitbox, 2)
