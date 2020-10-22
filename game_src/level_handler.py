b44edb981e6012253e048b8fa382d0636ce65aea
Daniel <daniel.yngve@student.kyh.se>
2020-10-22 16:21:21 +0200

    Fixed mob spawn position

--- game_src/level_handler.py
+++ game_src/level_handler.py
@@ -24,12 +24,12 @@ class Lane:
 
 def create_level(level_number):
     if level_number == 1:
-        level = Level(lanes=[Lane(350, 5, False), Lane(400, 5, True), Lane(450, 5, False), Lane(500, 5, True)],
+        level = Level(lanes=[Lane(345, 5, False), Lane(390, 5, True), Lane(435, 5, False), Lane(485, 5, True)],
                       background_image=get_background_image(0),
-                      mobs=[Mob(0, 350, 80, 40, get_mob_sprite(False), False),
-                            Mob(500, 400, 80, 40, get_mob_sprite(True), True),
-                            Mob(0, 450, 80, 40, get_mob_sprite(False), False),
-                            Mob(600, 500, 80, 40, get_mob_sprite(True), True)],
+                      mobs=[Mob(0, 345, 80, 40, get_mob_sprite(False), False),
+                            Mob(500, 390, 80, 40, get_mob_sprite(True), True),
+                            Mob(0, 435, 80, 40, get_mob_sprite(False), False),
+                            Mob(600, 485, 80, 40, get_mob_sprite(True), True)],
                       goat=Goat(400, 200, 40, 30),
                       music=0,
                       spawn_timer=[1000, 2000, 1000, 2000],
