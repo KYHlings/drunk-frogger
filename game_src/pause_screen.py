56cd395d5fc45f52d9379670dc574dbb2b2be59c
Daniel <daniel.yngve@student.kyh.se>
2020-10-23 11:09:15 +0200

    pause screen

--- game_src/pause_screen.py
+++ game_src/pause_screen.py
@@ -1,22 +1,28 @@
 import pygame
 
-from game_src.window_handler import text_object
+from game_src.window_handler import text_object, screen
 
 
 def pause_screen():
     options = ["[C]ontinue", "[S]ettings", "[Q]uit Game"]
     option_ls = []
+    pause = "Pause Menu"
     large_text = pygame.font.Font("font_src/PAPYRUS.TTF", 20)
     for option in options:
         option_ls.append(text_object(option, large_text))
     while True:
-        screen = pygame.Surface([600, 400])
-
+        pause_window = pygame.Surface([350, 350])
+        text_surf, text_rect = text_object(pause, large_text)
+        text_rect.center = (400, 300)
         alt = 350
+        screen.blit(pause_window, (175, 250))
+        screen.blit(text_surf, text_rect)
         for alternative in option_ls:
             alternative[1].center = (350, alt)
             alt += 90
             screen.blit(alternative[0], alternative[1])
             for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    quit()
         pygame.display.update()
     return
