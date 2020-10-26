2c539f08cd60daf97d15f70092ad083f9368ba85
Jonathan Vernersson <jonathan.vernersson@student.kyh.se>
2020-10-26 12:21:04 +0100

    Reformated quiz_handler.py

--- quiz_ui/quiz_handler.py
+++ quiz_ui/quiz_handler.py
@@ -6,24 +6,26 @@ import json
 import requests
 
 from image.image_handler import get_get_sprite, get_get_quiz_sprite
-from game_src.window_handler import screen,text_object
+from game_src.window_handler import screen, text_object
 
-#loads quiz from json-file, in future will load from api.
+
+# loads quiz from json-file, in future will load from api.
 def get_quiz():
     url = requests.get("https://opentdb.com/api.php?amount=25&category=15&difficulty=medium&type=multiple")
     content = url.json(encoding='utf8')['results']
     shuffle(content)
     return content
 
-#unloads content from get_quiz function.
+
+# unloads content from get_quiz function.
 def quiz():
-    quiz_content=get_quiz()
+    quiz_content = get_quiz()
     for q in quiz_content:
-        return q["question"],q["correct_answer"],q["incorrect_answers"]
+        return q["question"], q["correct_answer"], q["incorrect_answers"]
 
 
 def quiz_window(quiz):
-    #takes quiz function and draws on screen.
+    # takes quiz function and draws on screen.
     question, rightanswers, wronganswers = quiz
     question_list = [rightanswers]
     for wronganswer in wronganswers:
@@ -42,18 +44,18 @@ def quiz_window(quiz):
         for alternative in question_list:
             alternatives_text.append(text_object(f"{altnr}:{alternative}", large_text))
             altnr -= 1
-        #checks players answers
+        # checks players answers
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 for i in range(len(question_list)):
                     if event.key == keys[i]:
                         return question_list[i] == rightanswers
         screen.blit(get_get_quiz_sprite(), (100, 250))
-        screen.blit(message_window, (0, 400))
+        screen.blit(message_window, (0, 375))
         screen.blit(text_surf, text_rect)
-        alt_pos = 575
+        alt_pos = 565
         for alternative_text in alternatives_text:
-            alternative_text[1].center = (250, alt_pos)
+            alternative_text[1].topleft = (50, alt_pos)
             alt_pos -= 30
             screen.blit(alternative_text[0], alternative_text[1])
         pygame.display.update()
