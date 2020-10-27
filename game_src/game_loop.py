ee43022f291622c95958c8c0bfc865794f8ec9a8
david <david.pergament@student.kyh.se>
2020-10-27 16:49:00 +0100

    score in gameloop

--- game_src/game_loop.py
+++ game_src/game_loop.py
@@ -13,12 +13,13 @@ from sprites_classes.npc import Mob, Goat, Floating_mob
 from sprites_classes.player import Player
 from quiz_ui.quiz_handler import quiz_window, quiz
 from music_and_sound.sound_handler import get_level_music, get_goat_music, get_drunk_music
-from game_src.window_handler import screen, lose_window, win_window
+from game_src.window_handler import screen, lose_window, win_window, draw_text, font1, text_colour
 
 
 # This function updates the window with sprites_classes each loop
 def redraw_window(animals, wise_goat, dead_frog, background_image, lanes, floating_lanes, score):
     screen.blit(background_image, (0, 0))
+    draw_text(f"score:{score}",font1,text_colour )
 
     life_x = 10
     beer_y = 285
