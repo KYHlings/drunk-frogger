2a11f9a7a953940924b336e7e8f56b78d089962a
Andblad <tobbe_andblad@hotmail.com>
2020-10-29 13:32:29 +0100

    score file updates on command

--- game_src/score_handler.py
+++ game_src/score_handler.py
@@ -11,17 +11,18 @@ def score_by_player_position(player_y, last_y, score):
 
 
 def get_score():
-    score_ls = json.loads(Path("game_src/highscore.json").read_text(encoding='utf8'))
+    score_ls = json.loads(Path("highscore.json").read_text(encoding='utf8'))
     return score_ls
 
 
 def high_score_list(score):
     score_ls = get_score()
-    if score_ls[10]["score"] < score:
-        score_ls[10]["score"] = score
+    if score_ls[9]["score"] < score:
+        score_ls[9]["score"] = score
         name = input("enter name: ")
-        score_ls[10]["name"] = name
+        score_ls[9]["name"] = name
+        sorted_score = sorted(score_ls,key=lambda s:s['score'],reverse=True)
+        Path("highscore.json").write_text(json.dumps(sorted_score),encoding='utf8')
 
-
-
+high_score_list(2300)
 
