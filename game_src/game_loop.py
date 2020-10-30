6138216fe3e9c451d29c41694590e980baf26943
Elizabeth K <elizabeth.kallioniemi@student.kyh.se>
2020-10-30 17:21:00 +0100

    Next person plz fix it! Lanes

--- game_src/game_loop.py
+++ game_src/game_loop.py
@@ -71,7 +71,7 @@ def game_loop(sound_fx, volume):
     dead_frog = Dead_Frog()
     pygame.display.set_caption("Drunk Frogger")
 
-    level_number = 1
+    level_number = 2
     score = 0
     while True:
         level_title_window(level_number)
