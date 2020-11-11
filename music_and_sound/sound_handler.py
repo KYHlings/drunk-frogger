3e300907fce0c3d0d11b1389b2a533b248479881
Lunnehaxx <stairwaytobalken@gmail.com>
2020-11-11 08:53:40 +0100

    Commit title song.

--- music_and_sound/sound_handler.py
+++ music_and_sound/sound_handler.py
@@ -54,7 +54,8 @@ def music_volume(volume):
 # Each function gets a specific soundfile to load
 
 def get_title_music():
-    pygame.mixer.music.load("music_and_sound/music_src/df_theme_music2.mp3")
+    #pygame.mixer.music.load("music_and_sound/music_src/df_theme_music2.mp3")
+    pygame.mixer.music.load("music_and_sound/music_src/new_music/mahna_rapsody.mp3")
     return pygame.mixer.music.play(-1)
 
 
